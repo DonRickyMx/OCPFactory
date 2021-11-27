869320e735762bb2aa523b0de9f09632e4bf3ba4
Core i7 <Core i7@DON-RICKY>
2021-11-27 09:11:03 -0600

    Add project files.

---
+++ OCPFactory/DbFactory.cs
@@ -0,0 +1,43 @@
+﻿namespace OCPFactory {
+  public class DbFactory {
+    Dictionary<DbTipo, IDb> _dataBases = new();
+
+    public DbFactory() => SetUp();
+
+    // Es el único lugar donde se modifica para agregar o quitar opciones
+    void SetUp() {
+      _dataBases.Add(DbTipo.Sql, new SqlDb("Sql_ConnectionString"));
+      _dataBases.Add(DbTipo.MySql, new MySqlDb("MySql_ConnectionString"));
+    }
+
+    // Factory Abierto y Cerrado.
+    public IDb Crea(DbTipo tipo) { 
+      if (!ExisteDb(tipo)) throw new InvalidOperationException($"DbTipo invalido {tipo}");
+      return _dataBases[tipo];
+    }
+
+    bool ExisteDb(DbTipo tipo) => _dataBases.Any(db => db.Key == tipo);
+  }
+
+  public enum DbTipo {
+    Sql,
+    MySql,
+    Oracle
+  }
+
+  public interface IDb {
+    string TellType();
+  }
+
+  public class SqlDb : IDb {
+    string _connectionString;
+    public SqlDb(string connectionString) => _connectionString = connectionString;
+    public string TellType() => $"SqlDb, {_connectionString}";
+  }
+
+  public class MySqlDb : IDb {
+    string _connectionString;
+    public MySqlDb(string connectionString) => _connectionString = connectionString;
+    public string TellType() => $"MySqlDb, {_connectionString}";
+  }
+}
---
+++ OCPFactory/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OCPFactory;
+
+Console.WriteLine("OCP DataBse Factory");
+Console.WriteLine();
+
+var dbFactory = new DbFactory();
+
+var db = dbFactory.Crea(DbTipo.Sql);
+Console.WriteLine($"DbType: {db.TellType()}");
+
+db = dbFactory.Crea(DbTipo.MySql);
+Console.WriteLine($"DbType: {db.TellType()}");
+
+try{
+  db = dbFactory.Crea(DbTipo.Oracle);
+  Console.WriteLine($"DbType: {db.TellType()}");
+}
+catch (Exception e){
+  Console.WriteLine(e.Message);
+}
